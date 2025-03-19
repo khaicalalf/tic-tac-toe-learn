d3b4ff689eb4547a949ce4868855400819628701
Muhammad Ghufron Khaical Alfaris <alfaris.mgk@gmail.com>
2025-03-19 14:29:49 +0700

    update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import { useState } from 'react'
 
+function Square(){
+  return <button className="square">X</button>;
+}
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div className="App">
-      
+      <Square />
     </div>
   )
 }
