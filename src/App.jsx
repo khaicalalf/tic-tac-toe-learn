e8597f711296db435564228417546e5417fbebb6
Muhammad Ghufron Khaical Alfaris <alfaris.mgk@gmail.com>
2025-03-24 23:20:12 +0700

    make a board

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,25 @@
 import { useState } from 'react'
 
-function Square(){
-  return <button className="square">X</button>;
+function Square(value, onSquareClick){
+  return <button className="square" onClick={onSquareClick}>{value}</button>;
 }
 
-function App() {
-  const [count, setCount] = useState(0)
+function Board() {
+  const [squares, setSquares] = useState(Array(9).fill(null));
 
   return (
-    <div className="App">
-      <Square />
+    <div className="board">
+      <Square value={squares[0]}/>
+      <Square value={squares[1]}/>
+      <Square value={squares[2]}/>
+      <Square value={squares[3]}/>
+      <Square value={squares[4]}/>
+      <Square value={squares[5]}/>
+      <Square value={squares[6]}/>
+      <Square value={squares[7]}/>
+      <Square value={squares[8]}/>
     </div>
   )
 }
 
-export default App
+export default Board
